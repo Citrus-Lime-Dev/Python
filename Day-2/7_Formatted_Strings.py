7156dd1ed865a8f90eb182f4a7810e5c9647388a
Citrus-Lime-Dev <citruslimekc@gmail.com>
2026-07-01 12:34:13 +0530

    Create 7_Formatted_Strings.py

---
+++ Day-2/7_Formatted_Strings.py
@@ -0,0 +1,14 @@
+score = 0
+print(f"Original Score: {score}")
+
+score += 10
+print(f"Updated Score: {score}")
+
+score -= 1
+print(f"Score after a foul: {score}")
+
+score *= 100
+print(f"Score after points: {score}")
+
+score /= 10
+print(f"Final Score: {int(score)}")
