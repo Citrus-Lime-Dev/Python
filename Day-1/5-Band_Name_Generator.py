40e11401ce13ebe0f7f17228896b86866f3de8cf
Citrus-Lime-Dev <citruslimekc@gmail.com>
2026-07-01 07:23:55 +0530

    Create 5-Band_Name_Generator.py

---
+++ Day-1/5-Band_Name_Generator.py
@@ -0,0 +1,6 @@
+#Band Name Generator
+
+print("Welcome to Band Name Generator Program")
+city = input("Which city you grew up into? \n")
+pet = input("What type of pet do you like? \n")
+print("Your band name could be "+city+" "+pet)
