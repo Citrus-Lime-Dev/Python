1a0bbbdaa1c147bb9e972b686b54642e901fedf9
Citrus-Lime-Dev <citruslimekc@gmail.com>
2026-07-01 05:49:30 +0530

    Create 2_String_Manipulation.py

---
+++ Day-1/2_String_Manipulation.py
@@ -0,0 +1,8 @@
+# We have manipulated the sequence of strings using escape character - \n.
+# "+" operator is for concatenation of strings.
+# print() automatically adds a \n at the end of every statement.
+
+print("Hello World!\nHello World!\nHello World!\nHello World!")
+
+print("Hello" + " " +"Kalyan")
+print("Welcome to the world of Python")
