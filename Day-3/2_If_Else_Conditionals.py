1089db04128ca4006ff8800932db25103e261e83
Citrus-Lime-Dev <citruslimekc@gmail.com>
2026-07-01 19:07:19 +0530

    Create 2_If_Else_Conditionals.py

---
+++ Day-3/2_If_Else_Conditionals.py
@@ -0,0 +1,8 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height > 120:
+    print("Ypu are in for the exciting ride!")
+
+else:
+    print("Sorry you have to grow taller before you can ride!")
