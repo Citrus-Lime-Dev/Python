d3ef950f77fa6e0fb0a4c59236ad5ad40042bf0e
Citrus-Lime-Dev <citruslimekc@gmail.com>
2026-07-01 08:58:26 +0530

    Create 4_Mathematical_Operators.py

---
+++ Day-2/4_Mathematical_Operators.py
@@ -0,0 +1,11 @@
+#Mathematical Operators = + , - , * , / , ** 
+print(1 + 2)
+print(25 - 2)
+print(100 / 3)
+print(100 // 3)
+print(66 ** 3)
+
+#PEMDASLR - Parentheses, Exponents, Multiplication/Division, Addition/Subtraction , Left to Right Evaluation
+
+print(3 * 3 + 3 / 3 - 3) -> 7.0
+print(3 * (3 + 3) / 3 - 3) -> 3.0
