10d795885290bee379505b876a2f2325b5f1b5fe
Citrus-Lime-Dev <citruslimekc@gmail.com>
2026-07-01 18:55:57 +0530

    Create 1_Comparison_Operators.py

---
+++ Day-3/1_Comparison_Operators.py
@@ -0,0 +1,23 @@
+a = 15
+b = 20
+c = 30
+
+print("--- 1. COMPARISON OPERATORS ---")
+
+print(a == 15) -> True
+print(a > b) -> False
+print(a < b) -> True
+print(a >= b) -> False
+print(a <= b) -> True
+print(a == c) -> False
+print(a != c) -> True
+
+print("\n--- 2. LOGICAL OPERATORS ---")
+
+print(a>b and a>c) -> False
+print(b>c and b>a) -> False
+print(a>b or b<c) -> True
+print(b<c or b>a) -> True
+print(a!=b and b!=c) -> True
+print(a==b and b==c) -> False
+
