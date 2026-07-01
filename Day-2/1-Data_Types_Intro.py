2df16e3ba463a1d7957522d184cd746d862d5b8e
Citrus-Lime-Dev <citruslimekc@gmail.com>
2026-07-01 07:58:30 +0530

    Create 1-Data_Types_Intro.py

---
+++ Day-2/1-Data_Types_Intro.py
@@ -0,0 +1,12 @@
+#In Python, a data type classifies the kind of value a variable holds, telling the interpreter how to store, manage, and execute operations on that data
+
+#Strings - Any collection of characters which is in between " "(double quotes)
+#Integers - Any numbers without decimals
+#Floats - Decimal numbers
+#Booleans - True or False value
+
+#Subscripting
+print("Hello"[0]) -> H
+print("Hello"[-1]) -> o
+print("Hello"[4]) -> o
+print("Hello"[100]) -> Array Index Out of Bounds Error
