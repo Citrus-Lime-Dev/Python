fd0f2d963d81c891b62c1df7f57a6cf5b9c148ea
Citrus-Lime-Dev <citruslimekc@gmail.com>
2026-07-01 07:12:42 +0530

    Create 4-Python_Variables.py

---
+++ Day-1/4-Python_Variables.py
@@ -0,0 +1,12 @@
+#Variable - Placeholder or piece of data which holds values that can be later mulitple times using their names.
+
+name = "Kalyan"
+dob = 1995
+city = "Hyderabad"
+age = 31
+
+name = input("What is your name? ")
+print(name)
+print(len(name)) #len function is used to calculate the number of characters in a string.
+
+print(len(input("What is your naam? ")))
