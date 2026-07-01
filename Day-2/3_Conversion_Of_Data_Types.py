2b2a079f18f21c81089e8b41ce3dea1432be184e
Citrus-Lime-Dev <citruslimekc@gmail.com>
2026-07-01 08:23:06 +0530

    Create 3_Conversion_Of_Data_Types.py

---
+++ Day-2/3_Conversion_Of_Data_Types.py
@@ -0,0 +1,20 @@
+#Explicit Conversion of Data types
+print(int(True)) -> 1
+print(int(False)) -> 0
+print(int(2.56)) -> 2
+print(int("143")) -> 143
+# print(int("abc")) -> Produces error 
+
+print(float(2)) -> 2.0
+print(float(True)) -> 1.0
+print(float("123")) -> 123.0
+# print(float("abc")) -> Produces error 
+
+print(str(12345)) -> "12345"
+print(str(True)) -> "1"
+print(str(2.56)) -> "2.56"
+
+print(bool(123)) -> True
+print(bool("")) -> False
+print(bool(" ")) -> True
+print(bool("abc")) -> True
