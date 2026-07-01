06b05320976fd5466820a46fc9f38510951a8ce3
Citrus-Lime-Dev <citruslimekc@gmail.com>
2026-07-01 08:14:25 +0530

    Create Data_Types_In_Action.py

---
+++ Day-2/Data_Types_In_Action.py
@@ -0,0 +1,9 @@
+#type() is used to print the data type of any variable or value in Python
+
+print(len("12345")) -> 5
+print(type(len("12345"))) -> Integer
+
+print(type("Kalyan")) -> String
+print(type(123)) -> Integer
+print(type(3.1415)) -> float
+print(type(True)) -> boolean
