cfba9d909a9a8153c60a5c3709de2bf01cedc8e2
Citrus-Lime-Dev <citruslimekc@gmail.com>
2026-07-02 07:04:27 +0530

    Create 2_Modules_In_Python.py

---
+++ Day-4/2_Modules_In_Python.py
@@ -0,0 +1,10 @@
+A Python module is simply a file containing Python code—including functions, classes, and variables—with a .py extension that you can reuse across different programs. 
+The core purpose of a module is to break down large programs into smaller, manageable, and organized files, preventing you from rewriting or copy-pasting code.
+
+Python modules broadly fall into three categories :
+
+Built-in Modules: Pre-installed libraries included with Python (e.g., math, sys, random).
+Third-Party Modules: External libraries downloaded using package managers like pip (e.g., pandas, requests).
+User-Defined Modules: Custom .py files created by you to organize your own application logic.
+
+
