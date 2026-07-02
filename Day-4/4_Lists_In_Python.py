08859aa31549e2feed39a6a40b5851197f15f6c1
Citrus-Lime-Dev <citruslimekc@gmail.com>
2026-07-02 08:23:58 +0530

    Create 4_Lists_In_Python.py

---
+++ Day-4/4_Lists_In_Python.py
@@ -0,0 +1,18 @@
+A Python list is a built-in, mutable, and ordered collection of items enclosed in square brackets [].
+It is one of Python's most versatile data structures, capable of storing elements of different data types 
+simultaneously, including strings, integers, and even other lists.
+
+Core Characteristics
+
+Ordered: Items maintain their specific insertion order.
+Mutable: Elements can be added, updated, or removed after creation.
+Indexed: Elements are accessed via zero-based indexing (first item is [0]).
+Allow Duplicates: Multiple elements can hold the exact same value.
+
+Examples: 
+fruits = ["apple", "banana", "cherry"] 
+numbers = list((1, 2, 3))
+zeros = [0] * 4
+mixed = ["Python", 3.14, True]
+
+-> List items can be accessed via index or offset.
