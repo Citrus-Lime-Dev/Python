9640ea3d1ae99e298f196d5b6ca52b23da99edbc
Citrus-Lime-Dev <citruslimekc@gmail.com>
2026-07-02 17:22:47 +0530

    Create 1_Loops.py

---
+++ Day-5/1_Loops.py
@@ -0,0 +1,12 @@
+Loops in Python are control flow structures used to repeatedly execute a block of code as long as a condition is met 
+or until all elements in a sequence have been processed. 
+They eliminate the need to write duplicate code for repetitive tasks, reducing bugs and saving development time.
+
+There are mainly 2 loops in Python: for and while
+  
+A for loop iterates over a sequential collection (like a list, tuple, string, dictionary, or a generated range) and executes the code block once for each item. 
+You use this loop when you know the number of iterations beforehand or when you are processing items in a collection.
+  
+A while loop repeatedly executes code as long as a given boolean condition evaluates to True. 
+You use this loop when you do not know exactly how many times the code needs to run beforehand.
+
