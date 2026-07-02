b7760c57b6879b87af0721b9276d2ed9245e414b
Citrus-Lime-Dev <citruslimekc@gmail.com>
2026-07-02 17:25:39 +0530

    Create 2_For_Loop.py

---
+++ Day-5/2_For_Loop.py
@@ -0,0 +1,12 @@
+#Looping over a collection
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+
+#Looping over a range
+for num in range(5):
+  print(num)
+
+#Looping over a string
+for letter in "Python":
+  print(letter)
